66d3fd4fcdb842f46c17c497ee782b0326221795
LJ Spencer <hockeyduck3@gmail.com>
2020-05-15 22:29:55 -0600

    Added fieldValidation function, also added the code for the addEmployee function

--- server.js
+++ server.js
@@ -20,16 +20,26 @@ connection.connect((err) => {
     if (err) throw err;
 
     init();
-})
+});
+
+const fieldValidation = async input => {
+    if (input.trim() === '') {
+       return 'Field cannot be left blank';
+    }
+
+    return true;
+}
 
 // First function to run
 function init() {
-    inquirer.prompt({
+    inquirer
+     .prompt({
         type: 'list',
         name: 'firstQuestion',
         message: 'What would you like to do?',
         choices: ['View all employee\'s', 'Add new employee', 'Remove employee', 'Exit']
-    }).then(function(answer) {
+     })
+     .then(function(answer) {
         switch(answer.firstQuestion) {
             case 'View all employee\'s':
                 viewAllEmployee();
@@ -44,11 +54,53 @@ function init() {
                 console.log('See ya later!');
                 connection.end();
         }
-    });
+     });
 }
 
 function viewAllEmployee() {}
 
-function addNewEmployee() {}
+function addNewEmployee() {
+    const questions = [
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'What is the employee\'s first name?',
+            validate: fieldValidation
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the employee\'s last name?',
+            validate: fieldValidation
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            message: 'Who is the employee\'s manager?',
+            choices: ['John doe', 'Jane doe']
+        }
+    ];
+
+    inquirer
+     .prompt(questions)
+     .then(function(answer) {
+        connection.query(
+            'INSERT INTO employee SET ?',
+            {
+                first_name: answer.firstName,
+                last_name: answer.lastName,
+                role_id: 1,
+                manager_id: 1
+            },
+            function(err) {
+                if (err) throw err;
+
+                console.log(`${answer.firstName} ${answer.lastName} has been added to the Employee database!`);
+
+                init();
+            }
+        )
+     });
+}
 
 function removeEmployee() {}
