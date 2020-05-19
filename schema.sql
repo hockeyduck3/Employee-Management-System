cbd1509389b7033f29830279cb7e16828b37eeb8
LJ Spencer <hockeyduck3@gmail.com>
2020-05-19 15:04:39 -0600

    Added some Default values

--- schema.sql
+++ schema.sql
@@ -26,3 +26,10 @@ CREATE TABLE employee (
     manager_id INT,
     PRIMARY KEY (id)
 );
+
+-- Default values that can be changed later.
+INSERT INTO department (name)
+VALUES ("Sales"), ("Finance"), ("Engineering"), ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1), ("Sales Person", 80000, 1), ("Accountant", 125000, 2), ("Lead Engineer", 150000, 3), ("Software Engineer", 125000, 3), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
