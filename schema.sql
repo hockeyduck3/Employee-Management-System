bf3041973500f02d49a627ca01bdb211e6989f40
LJ Spencer <hockeyduck3@gmail.com>
2020-05-20 00:45:15 -0600

    Added another default list to the employee table

--- schema.sql
+++ schema.sql
@@ -33,3 +33,6 @@ VALUES ("Sales"), ("Finance"), ("Engineering"), ("Legal");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1), ("Sales Person", 80000, 1), ("Accountant", 125000, 2), ("Lead Engineer", 150000, 3), ("Software Engineer", 125000, 3), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 3, null), ("Jane", "Doe", 5, null);
